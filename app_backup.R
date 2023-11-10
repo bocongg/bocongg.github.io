ddfcc4407cce750dd26ae466ea946b95b06f6b50
bocongg <lew.bocong@u.nus.edu>
2023-11-10 23:01:18 +0800

    updated website with latest data visualisations

---
+++ app_backup.R
@@ -0,0 +1,352 @@
+library(shiny)
+library(plotly)
+library(gridlayout)
+library(bslib)
+library(DT)
+library(tidyverse)
+library(ggplot2)
+library(plyr)
+library(lubridate)
+library(zoo)
+library(shinyBS)
+library(bsicons)
+
+### Tidy the dataset
+wait_times <- read_csv("test_waiting_time.csv")
+
+bed_occ <- read_csv("test_BOR.csv")
+
+EMD_attendance <- read_csv("test_EMD_attendance.csv")
+
+
+tidy_wait_times <- wait_times %>%
+  separate(col = Date, into = c("Day_wk", "Date"), sep = ", ") %>%
+  mutate(new_Date_format = 
+           parse_date_time(Date, orders = c("mdy", "dmy", "ymd"))) %>%
+  mutate(Dates = format(new_Date_format, "%d/%m/%Y")) %>%
+  separate(col = Date, into = c("Day", "Month", "Year"), sep = "/") %>%
+  gather(key = hospital, value = wait_duration, 5:12 ) %>%
+  select(Day_wk, Dates, Day, Month, Year, hospital, wait_duration)
+
+tidy_bed_occ <- bed_occ %>%
+  filter(Years == 2023) %>%
+  mutate(new_Date_format = 
+           parse_date_time(Date, orders = c("mdy", "dmy", "ymd"))) %>%
+  mutate(Dates = format(new_Date_format, "%d/%m/%Y")) %>%
+  gather(key = hospital, value = occ_rate, 3:10) %>%
+  separate(col = occ_rate, into = c("occ_rate"), sep = "%") %>%
+  mutate(occ_rate_num = as.numeric(occ_rate)) %>%
+  select(occ_rate_num)
+
+tidy_EMD_attendance <- EMD_attendance %>%
+  separate(col = Date, into = c("Day_wk", "Date"), sep = ", ") %>%
+  mutate(new_Date_format = 
+           parse_date_time(Date, orders = c("mdy", "dmy", "ymd"))) %>%
+  mutate(Dates = format(new_Date_format, "%d/%m/%Y")) %>%
+  separate(col = Date, into = c("Day", "Month", "Year"), sep = "/") %>%
+  gather(key = hospital, value = attendance, 5:12) %>%
+  select(attendance)
+
+final_dataset <- cbind(tidy_wait_times,tidy_bed_occ,tidy_EMD_attendance)
+
+final_dataset <- 
+  final_dataset %>%
+  transform(Day = as.numeric(Day)) %>%
+  mutate(new_Date_format = 
+           parse_date_time(Dates, orders = c("mdy", "dmy", "ymd"))) %>%
+  mutate(week_num = as.numeric(format(new_Date_format, "%U"))) %>%
+  mutate(fullmonth = format(new_Date_format, "%b")) %>%
+  mutate(yearmonth = as.yearmon(new_Date_format)) %>%
+  mutate(yearmonthf = factor(yearmonth)) %>%
+  ddply(.(yearmonthf), transform, monthweek=5-week_num+min(week_num)) %>%
+  select(-new_Date_format,-yearmonth,-yearmonthf)
+
+### Define shiny modular functions
+sliderInputdate <- function(id,setvalue){
+  sliderInput(id,label = "",min = 1,max = 31,value = setvalue)
+}
+
+selectInputdate <- function(id, setlabel,choices = list(
+  "January" = "01",
+  "February" = "02",
+  "March" = "03",
+  "April" = "04",
+  "May" = "05",
+  "June" = "06",
+  "July" = "07",
+  "August" = "08",
+  "September" = "09")){
+  selectInput(id,label = setlabel,choices = choices)
+}
+
+checkboxGroupInputhosp <- function(id, setlabel, choice, selected = c("CGH", "SGH", "SKH")){
+  setlabel <- switch(
+    id,
+    "SH" = "Singhealth",
+    "NUHS" = "National University Health System",
+    "NHG" = "National Healthcare Group"
+  )
+  choices <- switch(
+    id,
+    "SH" = list("CGH" = "CGH", "SGH" = "SGH", "SKH" = "SKH"),
+    "NUHS" = list("AH" = "AH", "NTFGH" = "NTFGH", "NUH" = "NUH(A)"),
+    "NHG" = list("KTPH" = "KTPH", "TTSH" = "TTSH")
+  )
+  
+  checkboxGroupInput(id, label = setlabel, choices = choices, selected = selected)
+}
+
+plotlyOutputgrp <- function(datagrp, plotname, grpid = " "){
+  grpid <- switch(
+    datagrp,
+    "Waiting Time" = "WT",
+    "Bed Occupancy Rate" = "BOR",
+    "EMD Attendance" = "EMD"
+  )
+  
+  plottype = paste(grpid, plotname, sep = "")
+  plotlyOutput(outputId = plottype)
+}
+
+nav_panelplot <- function(datagrp, tabicon){
+  nav_panel(
+    title = datagrp,
+    grid_container(
+      layout = c("linechart","boxplot"),
+      gap_size = "10px",
+      col_sizes = c("1fr"),
+      row_sizes = c("1fr","1fr"),
+      grid_card(
+        area = "linechart",
+        full_screen = TRUE,
+        card_body(plotlyOutputgrp(datagrp, plotname = "linechart"))
+      ),
+      grid_card(
+        area = "boxplot",
+        full_screen = TRUE,
+        card_body(plotlyOutputgrp(datagrp, plotname = "boxplot"))
+      )
+    ),
+    icon = icon(tabicon)
+  )
+}
+
+plotlinedata <- function(mydata, x, startmthInput, startdateId, endmthInput, enddateId, SH, NUHS, NHG) {
+  
+  labelunit <- switch(
+    x,
+    "Waiting Time" = "hrs",
+    "Bed Occupancy Rate" = "%",
+    "EMD Attendance" = "patients"
+  )
+  ylabel <- switch(
+    x,
+    "Waiting Time" = "Hours (h)",
+    "Bed Occupancy Rate" = "BOR (%)",
+    "EMD Attendance" = "No. of patients"
+  )
+  
+  line <- mydata %>%
+    filter(Month >= startmthInput &
+             Day >= startdateId &
+             Month <= endmthInput &
+             Day <= enddateId &
+             hospital %in% c(SH, NUHS, NHG)) %>%
+    ggplot(aes(x = Dates,
+               y = switch(x,"Waiting Time" = wait_duration,"Bed Occupancy Rate" = occ_rate_num,"EMD Attendance" = attendance),
+               text = paste(switch(x,"Waiting Time" = wait_duration,"Bed Occupancy Rate" = occ_rate_num,"EMD Attendance" = attendance),
+                            labelunit))) + 
+    geom_point(size = 0.75) + 
+    geom_line(aes(group = hospital, color = hospital)) +
+    labs(x = "Date",
+         y = ylabel,
+         colour = "Hospital",
+         caption = "Source: Ministry of Health") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  
+  ggplotly(line, tooltip = "text") %>%
+    config(displayModeBar = FALSE) %>%
+    layout(dragmode = FALSE)
+}
+
+plotboxdata <- function(mydata, x, startmthInput, startdateId, endmthInput, enddateId, SH, NUHS, NHG) {
+  
+  labelunit <- switch(
+    x,
+    "Waiting Time" = "hrs",
+    "Bed Occupancy Rate" = "%",
+    "EMD Attendance" = "patients"
+  )
+  ylabel <- switch(
+    x,
+    "Waiting Time" = "Hours (h)",
+    "Bed Occupancy Rate" = "BOR (%)",
+    "EMD Attendance" = "No. of patients"
+  )
+  
+  box <- mydata %>% 
+    filter(Month >= startmthInput &
+             Day >= startdateId &
+             Month <= endmthInput &
+             Day <= enddateId &
+             hospital %in% c(SH, NUHS, NHG)) %>%
+    transform(hospital = as.factor(hospital)) %>%
+    ggplot(aes(x = fct_reorder(hospital, desc(hospital)),
+               y = switch(x,"Waiting Time" = wait_duration,"Bed Occupancy Rate" = occ_rate_num,"EMD Attendance" = attendance))) + 
+    geom_boxplot(aes(fill = hospital)) + 
+    labs(x = "Hospital",
+         y = ylabel,
+         caption = "Source: Ministry of Health") +
+    theme(legend.position = "none") +
+    coord_flip()
+  
+  ggplotly(box, tooltip = "text") %>%
+    config(displayModeBar = FALSE) %>%
+    layout(dragmode = FALSE)
+}
+
+
+### Main UI
+ui <- grid_page(
+  layout = c(
+    "header       header      ",
+    "datesettings hospsettings",
+    "tabset       tabset      "
+  ),
+  gap_size = "1px",
+  col_sizes = c("0.90fr","1.10fr"),
+  row_sizes = c("50px","270px","1fr"),
+  
+  grid_card_text(
+    area = "header",
+    content = "Data Dashboard",
+    alignment = "start",
+    is_title = FALSE
+  ),
+  
+  grid_card(
+    area = "datesettings",
+    card_header("Select Date:"),
+    card_body(
+      grid_container(
+        layout = c("startdate enddate"),
+        gap_size = "0px",
+        col_sizes = c("1fr","1fr"),
+        row_sizes = c("1fr"),
+        grid_card(
+          area = "startdate",
+          card_body(
+            selectInputdate("startmthInput",setlabel = "Start Date"),
+            sliderInputdate("startdateId",1)
+          )
+        ),
+        grid_card(
+          area = "enddate",
+          card_body(
+            selectInputdate("endmthInput",setlabel = "End Date"),
+            sliderInputdate("enddateId",31)
+          )
+        )
+      )
+    )
+  ),
+  
+  grid_card(
+    area = "hospsettings",
+    card_header("Select Hospital(s):"),
+    card_body(
+      grid_container(
+        layout = c("selecthosp1 selecthosp2 selecthosp3"),
+        row_sizes = c("1fr"),
+        col_sizes = c("1fr","1fr","1fr"),
+        gap_size = "0px",
+        grid_card(
+          card_header("Singhealth",
+                      tooltip(
+                        bs_icon("question-circle"),
+                        "CGH = Changi General Hospital,",
+                        "SGH = Singapore General Hospital,",
+                        "SKH = Sengkang General Hospital",
+                        placement = "right",
+                        ),
+                      ),
+          area = "selecthosp1",
+          card_body(
+            checkboxGroupInputhosp("SH")
+          )
+        ),
+        grid_card(
+          card_header("National University Health System",
+                      tooltip(
+                        bs_icon("question-circle"),
+                        "AH = Alexendra Hospital,",
+                        "NTFGH = Ng Teng Fong General Hospital,",
+                        "NUH = National University Hospital",
+                        placement = "right"
+                        )
+                      ),
+          area = "selecthosp2",
+          card_body(
+            checkboxGroupInputhosp("NUHS")
+          )
+        ),
+        grid_card(
+          card_header("National Healthcare Group",
+                      tooltip(
+                        bs_icon("question-circle"),
+                        "KTPH = Khoo Teck Puat Hospital,",
+                        "TTSH = Tan Tock Seng Hospital",
+                        placement = "right"
+                        )
+                      ),
+          area = "selecthosp3",
+          card_body(
+            checkboxGroupInputhosp("NHG")
+          )
+        )
+      )
+    )
+  ),
+  
+  grid_card(
+    area = "tabset",
+    card_body(
+      tabsetPanel(
+        nav_panelplot("Waiting Time", "clock"),
+        nav_panelplot("Bed Occupancy Rate", "bed"),
+        nav_panelplot("EMD Attendance", "person")
+      )
+    )
+  )
+)
+
+### Main server
+server <- function(input, output) {
+  
+  output$WTlinechart <- renderPlotly({
+    plotlinedata(final_dataset, x = "Waiting Time", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  output$WTboxplot <- renderPlotly({
+    plotboxdata(final_dataset, x = "Waiting Time", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  
+  output$BORlinechart <- renderPlotly({
+    plotlinedata(final_dataset, x = "Bed Occupancy Rate", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  output$BORboxplot <- renderPlotly({
+    plotboxdata(final_dataset, x = "Bed Occupancy Rate", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  
+  output$EMDlinechart <- renderPlotly({
+    plotlinedata(final_dataset, x = "EMD Attendance", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  output$EMDboxplot <- renderPlotly({
+    plotboxdata(final_dataset, x = "EMD Attendance", input$startmthInput, input$startdateId, input$endmthInput, input$enddateId, input$SH, input$NUHS, input$NHG)
+  })
+  
+}
+
+shinyApp(ui, server)
+
+
